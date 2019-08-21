7a109ad1fca1e0654fcc10ab50f780a71c53e0ac
Abbey Masters <abbeykmasters@gmail.com>
2019-08-21 14:30:31 -0700

    fix

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/events', (request, response) => {
     const latitude = request.query.latitude;
     const longitude = request.query.longitude;
 
-    eventbriteApi.getEvents(latitude, longitude)
+    eventbriteApi.listEvents(latitude, longitude)
         .then(events => {
             response.json(events);
         })
